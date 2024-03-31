4e1cb0ae8338b474b2eeeca48b497c3cecc06cd3
HaidarGhanem <haidar23ghanem@gmail.com>
2024-03-31 20:52:54 +0200

    new edited 3

--- app.js
+++ app.js
@@ -36,4 +36,4 @@ app.use('/dashboard/troubleshooting' , require('./routes/troubleshooting.js'))
 app.use('/dashboard/security' , require('./routes/security'))
 app.use('/dashboard/protocols' , require('./routes/protocols'))
 
-app.listen(process.env.PORT || 3000 , ()=>{console.log(`server is running on http://localhost/${PORT}/dashboard`)})
+app.listen(process.env.PORT, ()=>{console.log(`server is running`)})
